4e22e6081ba46bbc37db6433f54d62940948cfef
Aashish Singh Bisht <76493816+KewlAashish@users.noreply.github.com>
2022-09-15 19:58:46 +0530

    Add files via upload
    assignment one

---
+++ AssignmentOne/Main.java
@@ -0,0 +1,61 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+        // question 1
+        /*
+        Write a Java program to print addition and subtraction of two
+        integer numbers.
+        */
+        int a, b, c, d;
+        a = 10;
+        b = 15;
+        System.out.println(a + b);
+
+        c = 20;
+        d = 10;
+        System.out.println(c - d);
+        
+        // Question 2
+        /*
+        Write a Java program to print multiplication of two floating
+        point numbers.
+        */
+        
+        float e = 10, f = 2;
+        System.out.println(e*f);
+        
+        /*
+        Question 3
+        Write a Java program to compute quotient and remainder and
+        print it.
+        */
+        int quotient = 5 / 3;
+        int remainder = 5 % 3;
+        
+        System.out.println(quotient);
+        System.out.println(remainder);
+        
+        /*
+        Question 4:
+        Write a Java program to swap the values of integer number
+        with using temporary variable and without using temporary
+        variable
+        */
+        System.out.println("\n Initial value of a " + a);
+        System.out.println("\n Initial value of b " + b);
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("\n Final value of a " + a);
+        System.out.println("\n Final value of b " + b);
+        
+    }
+}
